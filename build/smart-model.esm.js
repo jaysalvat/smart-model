bd17b299ede05aea2e5ee197dfabc934cbd389ef
Jay Salvat <jay@jaysalvat.com>
2021-02-19 22:54:30 +0100

    Build v0.2.10

--- build/smart-model.esm.js
+++ build/smart-model.esm.js
@@ -3,7 +3,7 @@
 * SmartModel
 * Javascript object model
 * https://github.com/jaysalvat/smart-model
-* @version 0.2.9 built 2021-02-19 21:51:41
+* @version 0.2.10 built 2021-02-19 21:54:25
 * @license ISC
 * @author Jay Salvat http://jaysalvat.com
 */
