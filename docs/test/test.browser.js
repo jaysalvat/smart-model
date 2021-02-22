e89ea07abb419727a65198f5476f6b29de144f57
Jay Salvat <jay@jaysalvat.com>
2021-02-22 09:20:20 +0100

    Remove /docs

--- docs/test/test.browser.js
+++
@@ -1,7 +0,0 @@
-
-import SmartModel from 'https://unpkg.com/@jaysalvat/smart-model@latest/build/smart-model.esm.min.js'
-import test from './test.link.js'
-
-const { expect } = window.chai
-
-test(expect, SmartModel)
