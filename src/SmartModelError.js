75af5e63b575d801b9c90b50c9a07b01eee1186c
Jay Salvat <jay@jaysalvat.com>
2021-02-25 08:16:55 +0100

    Add source error message

--- src/SmartModelError.js
+++ src/SmartModelError.js
@@ -10,12 +10,14 @@ class SmartModelError extends Error {
 SmartModelError.throw = function (settings, code, message, property, source) {
   const shortCode = code.split(':')[0]
 
+  source = source && source.constructor.name
+
   if (settings.exceptions === true || (isPlainObject(settings.exceptions) && settings.exceptions[shortCode])) {
     throw new SmartModelError({
-      message,
+      message: `[${source}] ${message}`,
+      source: source,
       property,
-      code,
-      source: source && source.constructor.name
+      code
     })
   }
 }
